140e7f00c54337641bb8cb2d86e7ecd3fc02eb48
TendaiMare <tendaimare@yahoo.com>
2020-06-27 16:58:03 -0700

    Mobile nav changes

--- main.js
+++ main.js
@@ -1,13 +1,24 @@
 let hamburgerButton = document.querySelector(".toggle-button");
 let backdrop  = document.querySelector(".backdrop");
 let mobileNav = document.querySelector(".mobile-nav");
+let mobileNavLinks = document.querySelectorAll(".mobile-nav-link");
 
 backdrop.addEventListener("click",()=>{
-    mobileNav.classList.remove("open");
-    backdrop.classList.remove("open");
+    mobileNav.style.display="none";
+    backdrop.style.display="none";
 })
 
 hamburgerButton.addEventListener("click",()=>{
-    mobileNav.classList.add("open");
-    backdrop.classList.add("open");
+    mobileNav.style.display="block";
+    backdrop.style.display="block";
 })
+
+
+for (let i = 0 ; i < mobileNavLinks.length ; i++) {
+
+    mobileNavLinks[i].addEventListener("click",()=>{
+    mobileNav.style.display="none";
+    backdrop.style.display="none";
+})
+
+}
